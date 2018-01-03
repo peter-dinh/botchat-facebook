d7693539987522c2003a8cfa0b1437672be00066
dinh truong <dinhtruong018@gmail.com>
2018-01-03 15:48:19 +0700

    add

---
+++ botchat.py
@@ -0,0 +1,81 @@
+# -*- coding: UTF-8 -*-
+
+from fbchat import log, Client
+from fbchat.models import *
+import getpass
+
+# Subclass fbchat.Client and override required methods
+class Run_App(Client):
+
+    action = 1
+    def onMessage(self, mid, author_id, message, message_object, thread_id, thread_type, **kwargs):
+        self.markAsDelivered(author_id, thread_id)
+        self.markAsRead(author_id)
+
+        # if message_object.text == 'Talk with strangers!':
+        #     self.action = 2
+
+        if author_id == self.uid:
+            if message_object.text == 'Stop!':
+                self.action = 0
+                message_relay = 'Đã tiếp nhận yêu cầu!!'
+                relay = Message(text = message_relay)
+                self.send(relay, thread_id=thread_id, thread_type=thread_type)
+            if message_object.text == 'Start!':
+                self.action = 1
+                message_relay = 'Đã tiếp nhận yêu cầu!!'
+                relay = Message(text = message_relay)
+                self.send(relay, thread_id=thread_id, thread_type=thread_type)
+
+        if self.action == 0:
+            return
+        else:
+            log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
+            
+            # If you're not the author, echo
+            if author_id != self.uid:
+                user = self.fetchUserInfo(author_id)[author_id]
+                # Đánh dấu “Yêu thích” tin nhắn người gửi
+                self.reactToMessage(mid, MessageReaction.LOVE)
+
+                # message_relay = 'Vui lòng chờ trong giây lát!'
+                # relay = Message(text = message_relay)
+                # self.send(relay, thread_id=thread_id, thread_type=thread_type)
+
+                # message_relay = 'Thông tin người gửi: {}\n'.format(user.name)
+                # relay = Message(text = message_relay)
+                # self.send(relay, thread_id=thread_id, thread_type=thread_type)
+
+                # message_relay = 'Mã người gửi: {}\n'.format(user.uid)
+                # relay = Message(text = message_relay)
+                # self.send(relay, thread_id=thread_id, thread_type=thread_type)
+
+                # message_relay = 'Hình ảnh người gửi: {}'.format(user.photo)
+                # relay = Message(text = message_relay)
+                # self.send(relay, thread_id=thread_id, thread_type=thread_type)
+
+                # message_relay = 'Có phải bạn của chủ nhân: {}\n'.format(user.is_friend)
+                # relay = Message(text = message_relay)
+                # self.send(relay, thread_id=thread_id, thread_type=thread_type)
+
+                # message_relay = 'Giới tính: {}\n'.format(user.gender)
+                # relay = Message(text = message_relay)
+                # self.send(relay, thread_id=thread_id, thread_type=thread_type)
+
+                # message_relay = 'Địa chỉ profile: {}\n'.format(user.url)
+                # relay = Message(text = message_relay)
+                # self.send(relay, thread_id=thread_id, thread_type=thread_type)
+
+                message_relay = 'Bot: Đã nhận được tin nhắn từ "{}" với nội dung là: "{}"'.format(self.fetchThreadInfo(thread_id)[thread_id].name, message)
+                relay = Message(text = message_relay)
+                self.send(relay, thread_id=thread_id, thread_type=thread_type)
+
+                message_relay = 'Bot: Thư đã được gửi đến Peter! Bạn vui lòng chờ đợi sau ít phút'
+                relay = Message(text = message_relay)
+                self.send(relay, thread_id=thread_id, thread_type=thread_type)
+
+if __name__ == '__main__':
+    username = input("Username: ")
+    password = getpass.getpass("Password: ")
+    client = Run_App(username,password)
+    client.listen()
